0261b1b3e8371471b5f7811cc2ac45ec623bc234
Marek Matejovic <me@devmaroy.com>
2024-10-27 19:45:22 +0100

    Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const { mauve, violet } = require("@radix-ui/colors");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-
   theme: {
     container: {
       center: true,
